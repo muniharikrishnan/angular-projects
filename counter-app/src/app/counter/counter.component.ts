47f5d94c1f768aef77b4302beb11bf5c26af7926
Muni Hari Krishnan M <amruthavallia7@gmail.com>
2025-02-19 15:26:18 +0530

    created counter-app

---
+++ counter-app/src/app/counter/counter.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+
+  count=0
+  increment(){
+    this.count++;
+  }
+  decrement(){
+    if(this.count>0){
+      this.count--;
+    }
+  }
+}
